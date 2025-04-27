41b9791991b700247ac2ea6774f11b4ba46b49b8
Natalia-tonn <natali-tonn@web.de>
2025-04-27 11:49:47 +0200

    добавлен блок header-main-right

--- FE/src/components/Header.tsx
+++ FE/src/components/Header.tsx
@@ -10,31 +10,34 @@ const Header = () => {
           <img src="/logo.svg" alt="Logo" />
         </div>
 
-        <div className="header-right">
-          <div className="header-contacts">
-            <p>0174 345 44 19</p>
-            <p>tonn_andreas@web.de</p>
-            <p>Sessendrupweg 54</p>
-            <p>48161 Münster</p>
+        <div className="header-main-right">
+          <div className="header-right">
+            <div className="header-contacts">
+              <p>0174 345 44 19</p>
+              <p>tonn_andreas@web.de</p>
+              <p>Sessendrupweg 54</p>
+              <p>48161 Münster</p>
+            </div>
           </div>
-        </div>
-      </div>
+      
 
-      <div className="header-nav">
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/wertermittlung">Wertermittlung</NavLink>
-          </li>
-          <li>
-            <NavLink to="/immobilien">Immobilien</NavLink>
-          </li>
-          <li>
-            <NavLink to="/finanzierung">Finanzierung</NavLink>
-          </li>
-        </ul>
+        <div className="header-nav">
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/wertermittlung">Wertermittlung</NavLink>
+            </li>
+            <li>
+              <NavLink to="/immobilien">Immobilien</NavLink>
+            </li>
+            <li>
+              <NavLink to="/finanzierung">Finanzierung</NavLink>
+            </li>
+          </ul>
+        </div>
+        </div>
       </div>
     </header>
   );
