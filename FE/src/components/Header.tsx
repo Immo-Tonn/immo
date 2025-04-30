185a460f52938f24af34000dcfe56bada6252b9b
Kristin198689 <kristin198689@gmail.com>
2025-04-30 11:25:10 +0200

    -

--- FE/src/components/Header.tsx
+++ FE/src/components/Header.tsx
@@ -13,30 +13,38 @@ const Header = () => {
         <div className="header-main-right">
           <div className="header-right">
             <div className="header-contacts">
-              <p>0174 345 44 19</p>
-              <p>tonn_andreas@web.de</p>
-              <p>Sessendrupweg 54</p>
+              <p>
+                <img src="/phone.svg" alt="Phone" className="icon" />
+                0174 345 44 19
+              </p>
+              <p>
+                <img src="/mail.svg" alt="Mail" className="icon" />
+                tonn_andreas@web.de
+              </p>
+              <p>
+                <img src="/location.svg" alt="Location" className="icon" />
+                Sessendrupweg 54
+              </p>
               <p>48161 Münster</p>
             </div>
           </div>
-      
 
-        <div className="header-nav">
-          <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/wertermittlung">Wertermittlung</NavLink>
-            </li>
-            <li>
-              <NavLink to="/immobilien">Immobilien</NavLink>
-            </li>
-            <li>
-              <NavLink to="/finanzierung">Finanzierung</NavLink>
-            </li>
-          </ul>
-        </div>
+          <div className="header-nav">
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/wertermittlung">Wertermittlung</NavLink>
+              </li>
+              <li>
+                <NavLink to="/immobilien">Immobilien</NavLink>
+              </li>
+              <li>
+                <NavLink to="/finanzierung">Finanzierung</NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </header>
