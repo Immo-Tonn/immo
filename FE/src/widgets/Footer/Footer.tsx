3e1cba562caa2f837a38f5048ea795099abd39c9
Heorhii <91286488+GertSer@users.noreply.github.com>
2025-04-30 19:01:14 +0200

    feat: Footer added, u have to create a cute about-us sections!
    And u must to decide, which of the componets must be as SHARED

--- FE/src/widgets/Footer/Footer.tsx
+++ FE/src/widgets/Footer/Footer.tsx
@@ -1,9 +1,49 @@
+import { Link, useNavigate } from 'react-router-dom';
+import styles from './Footer.module.css';
+import logo from '@shared/assets/about-us/logo-white.svg';
 const Footer = () => {
+  const navigate = useNavigate();
+
+  const handleContactClick = () => {
+    navigate('/kontakt');
+  };
+
   return (
-    <footer
-      style={{ textAlign: 'center', padding: '1rem', background: '#f3f3f3' }}
-    >
-      © {new Date().getFullYear()} ImmoTonn GmbH. Alle Rechte vorbehalten.
+    <footer>
+      <div className={styles.topStripe}></div>
+      <div className={styles.footerContainer}>
+        <div className={styles.footerLeft}>
+          <img src={logo} alt="logo" className={styles.footerLogo} />
+          <ul className={styles.footerLinks}>
+            <li>
+              <Link to="/legalnotice">Impressum</Link>
+            </li>
+            <li>
+              <Link to="/privacypolicy">Datenschutz</Link>
+            </li>
+            <li>
+              <Link to="/cancellationpolicy">Widerrufsrecht</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className={styles.footerCenter}>
+          <p>Sessendrupweg 54</p>
+          <p>48161 Münster</p>
+          <p>Tel.: 0251 625 60 763</p>
+          <p>Mobil: 0174 345 44 19</p>
+          <p>www.tonn-versicherung.de</p>
+        </div>
+
+        <div className={styles.footerButtonWrapper}>
+          <button className={styles.contactButton} onClick={handleContactClick}>
+            Kontaktieren Sie uns
+          </button>
+        </div>
+      </div>
+      <div className={styles.bottomStripe}>
+        <p>Immo Tonn. All rights reserved 2025</p>
+      </div>
     </footer>
   );
 };
