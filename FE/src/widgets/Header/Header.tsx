0f72fda0671cfc8ae3d3d582c6a9a1dd9fc9bcaf
Heorhii <91286488+GertSer@users.noreply.github.com>
2025-05-04 23:25:24 +0200

    feat: added links to contact information in Header component to use links for phone, email, and address

--- FE/src/widgets/Header/Header.tsx
+++ FE/src/widgets/Header/Header.tsx
@@ -18,13 +18,20 @@ const Header = () => {
                 <div className={styles.contactIcon}>
                   <img src={telephone} alt="phone" className="icon" />
                 </div>
-                <div className={styles.contactText}>0174 345 44 19</div>
+                <a href="tel:01743454419" className={styles.contactText}>
+                  0174 345 44 19
+                </a>
               </li>
               <li>
                 <div className={styles.contactIcon}>
                   <img src={eMail} alt="mail" className="icon" />
                 </div>
-                <div className={styles.contactText}>tonn_andreas@web.de</div>
+                <a
+                  href="mailto:tonn_andreas@web.de"
+                  className={styles.contactText}
+                >
+                  tonn_andreas@web.de
+                </a>
               </li>
               <li>
                 <div className={styles.contactIcon}>
@@ -35,11 +42,21 @@ const Header = () => {
                     style={{ width: '47px', height: '49px' }}
                   />
                 </div>
-                <div className={styles.contactText}>Sessendrupweg 54</div>
+                <a
+                  href="https://www.google.com/maps/place/Sessendrupweg+54,+48161+M%C3%BCnster/@52.000885,7.5495001,17z/data=!3m1!4b1!4m6!3m5!1s0x47b9b0fb68b86337:0x6c01106fad5b0129!8m2!3d52.000885!4d7.552075!16s%2Fg%2F11bw3zj4mf?entry=ttu&g_ep=EgoyMDI1MDQzMC4xIKXMDSoASAFQAw%3D%3D"
+                  className={styles.contactText}
+                >
+                  Sessendrupweg 54
+                </a>
               </li>
               <li>
                 <div className={styles.contactIcon}></div>
-                <div className={styles.contactText}>48161 Münster</div>
+                <a
+                  href="https://www.google.com/maps/place/Sessendrupweg+54,+48161+M%C3%BCnster/@52.000885,7.5495001,17z/data=!3m1!4b1!4m6!3m5!1s0x47b9b0fb68b86337:0x6c01106fad5b0129!8m2!3d52.000885!4d7.552075!16s%2Fg%2F11bw3zj4mf?entry=ttu&g_ep=EgoyMDI1MDQzMC4xIKXMDSoASAFQAw%3D%3D"
+                  className={styles.contactText}
+                >
+                  48161 Münster
+                </a>
               </li>
             </ul>
           </div>
