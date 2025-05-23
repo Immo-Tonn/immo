7ed28be224fb4e8b4c446d87b6fa016a0a1b69a2
olgazakre <olga.zakrevska1980@gmail.com>
2025-05-23 17:41:03 +0200

    изменения номер обьекта

--- BE/src/models/RealEstateObjectsModel.ts
+++ BE/src/models/RealEstateObjectsModel.ts
@@ -50,7 +50,6 @@ const RealEstateObjectsSchema: Schema = new Schema<IRealEstateObjects>({
   },
   number: {
     type: String,
-    required: true,
     unique: true,
   },
   title: {
@@ -119,8 +118,27 @@ const RealEstateObjectsSchema: Schema = new Schema<IRealEstateObjects>({
   },
 });
 
+RealEstateObjectsSchema.pre<IRealEstateObjects>("save", function (next) {
+  if (!this.isNew || this.number) return next();
+
+  const date = this.dateAdded || new Date();
+
+  const year = date.getFullYear();
+  const month = `${date.getMonth() + 1}`.padStart(2, "0");
+  const day = `${date.getDate()}`.padStart(2, "0");
+  const hours = `${date.getHours()}`.padStart(2, "0");
+  const minutes = `${date.getMinutes()}`.padStart(2, "0");
+  const seconds = `${date.getSeconds()}`.padStart(2, "0");
+  const millis = `${date.getMilliseconds()}`.padStart(3, "0");
+
+  this.number = `${year}${month}${day}${hours}${minutes}${seconds}${millis}`;
+
+  next();
+});
+
 const RealEstateObjectsModel = mongoose.model<IRealEstateObjects>(
   "RealEstateObjects",
   RealEstateObjectsSchema
 );
+
 export default RealEstateObjectsModel;
