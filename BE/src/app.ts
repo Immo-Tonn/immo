cc7b9fbd0b5c2ac9efc0006fc843331a0d5e5834
Heorhii <91286488+GertSer@users.noreply.github.com>
2025-06-09 13:06:40 +0200

    I save unsaved editings

--- BE/src/app.ts
+++ BE/src/app.ts
@@ -40,8 +40,5 @@ const PORT: number | string = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 
 export default app;
