c9e950c06454cd613e33d449382165f1ec1c27bc
Ivan-Stierlitz <sa-ev@gmx.net>
2025-07-05 01:51:51 +0200

    control commit

--- FE/src/pages/Valuation/Valuation.tsx
+++ FE/src/pages/Valuation/Valuation.tsx
@@ -11,16 +11,3 @@ const Valuation = () => {
 };
 export default Valuation;
 
-import Competence from '@widgets/Competence/Competence';
-import HowIsGoing from '@widgets/HowIsGoing/HowIsGoing';
-import ValuationCTA from '@widgets/ValuationCTA/ValuationCTA';
-import HeroValuation from '@widgets/hero/HeroValuation/HeroValuation.module';
-const Valuation = () => {
-  return (
-    <>
-      <HeroValuation /> <ValuationCTA /> <Competence /> <HowIsGoing />
-    </>
-  );
-};
-export default Valuation;
-
