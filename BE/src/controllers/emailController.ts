a5baf630bd50e7f8301b0f9f5f86e25967e9de59
tritonJS826 <triton.68@mail.ru>
2025-08-04 19:10:04 +0200

    added logs

--- BE/src/controllers/emailController.ts
+++ BE/src/controllers/emailController.ts
@@ -40,6 +40,8 @@ export const sendContactEmail = async (req: Request, res: Response) => {
         .status(400)
         .json({ message: 'reCAPTCHA-Überprüfung fehlgeschlagen' });
     }
+
+    console.log("!!!!!", process.env.EMAIL_USER, process.env.EMAIL_PASS)
     const transporter = nodemailer.createTransport({
       host: 'smtp.strato.de',
       port: 465,
@@ -65,12 +67,18 @@ export const sendContactEmail = async (req: Request, res: Response) => {
 
     console.log('📤 Versende Nachricht an:', mailOptions.to);
 
+    // infinite request
     const info = await transporter.sendMail(mailOptions);
 
     console.log('✅ Nachricht gesendet:', info.response);
     res.status(200).json({ message: 'Nachricht wurde erfolgreich versendet' });
   } catch (error: any) {
-    console.error('❌ Fehler beim Mailversand:', error?.message || error);
+    console.error(
+      '❌ Fehler beim Mailversand:',
+      error.message,
+      error.code,
+      error.command,
+    );
     res.status(500).json({
       message: 'Serverfehler beim Versenden der Nachricht',
     });
